6f5b8f328dee4b4a29129962b6cf3d1a7473687a
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-08-02 14:29:01 +0530

    smart solution
    matematical formulae used here

--- pelindrome.py
+++ pelindrome.py
@@ -7,27 +7,7 @@
 
 
 
-j=0
 for i in range(1,int(input())+1):
-     #More than 2 lines will result in 0 score. Do not leave a blank line also
-    
-    k=1
-    m=1
-
-    while(k<=i+j):
-        if(k==i+j):
-            l='\n'
-        else:
-
-            l='' 
-        if(k>i):
-            m=m-1
-        
-        print(m,end=l),
-        
-        if(k<i):
-            m=m+1
-        k=k+1
-    j=j+1
+    print((10**i//9)**2)
     
 
