1ca7beed4192867c275ec4ad2c56eb06ece73afb
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-08-30 21:05:08 +0530

    updated

--- price.py
+++ price.py
@@ -1,8 +1,14 @@
+#here compute the minimum price from given list of items
+#here you have to select K items and l[J] must be purchased
+#return the minimum price to purchase K items given l[J] must be included.
+
+
 price=0
 N,K,J=[int(i) for i in input().split()]
 
 l = [int(i) for i in input().split()][:N]
 price=price+l[J-1]
+l.remove(l[J-1]
 l.sort()
 for i in range(0,K-1):
 	price=price+l[i]
