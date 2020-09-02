c21b7a2324d0f8804aea375f80b1fc3382f43074
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-09-02 21:17:08 +0530

    Update duplicate_word.py

--- duplicate_word.py
+++ duplicate_word.py
@@ -1,14 +1,11 @@
 # count the no. of duplicates word and print the count.
-
-
-
 dict={}
-
+#loop for taking the input words.
 for i in range(int(input())):
     #If input not in the dictionary, then add it
     #else increment the counter
     key = input()
-    if not key in dict.keys():
+    if not key in dict.keys(): #keys() returns keys used in the dictionary.
         dict.update({key : 1})
         continue
     dict[key] += 1
