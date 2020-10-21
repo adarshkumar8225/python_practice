85c58a94a2e619fdb848aefc7a498e8c21a44de6
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-10-21 13:00:15 +0530

    anargam i.e two string contains same set of alphabets

---
+++ anargam.py
@@ -0,0 +1,38 @@
+#here we have to find two string are anargam or not...... anargam means two string string 
+# have same number and type of character.....
+# i made two dictionary which keeys type of character as key and number of particular character as its value
+#we finally get two dictionary now if both dictionary are equal then print YES else print NO......
+
+
+t=int(input())
+res={}
+result={}
+for i in range(0,t):
+    (str_first)=str((input())
+    print(5)
+    print(str_second)
+    
+    
+    #all_freq = {} 
+  
+    #for keys in str_first: 
+        
+    #    res[keys] = res.get(keys, 0) + 1
+  
+    
+    #all_freqn = {} 
+  
+    #for keys in str_second: 
+        
+    #    result[keys] = result.get(keys, 0) + 1
+  
+    
+    
+    if(res==all_freqn) :
+        print("YES")
+    else:
+        print("NO")
+        
+    res.clear()
+    result.clear()
+
