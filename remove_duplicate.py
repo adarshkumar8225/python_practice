88a8679e5640de25f4f534adb6114ead8eb865ae
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-10-21 18:56:24 +0530

    only store first occurrence of a character

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,17 +1,16 @@
 	#keep only first occurance of string ex: geeksforgeeks will give geksfor
   #use dictionary and first occurance of a char will be the key .
   #if again some character occurs the delete that character.......
-  
   def removeDups(self, S):
-	     #code here
-		dict1={}
-		i=0
-		for keys in S:
-		    
-		    if(keys in dict1.keys()):
-		        S=S[:i]+S[i+1:]#deleting a char from S so i is not incremented.
-		        
-		    else:
-		        dict1[keys]=dict1.get(keys,0)+1
-		        i=i+1
-		return S
+	
+        #code here
+	dict1={}
+	M=""
+	for keys in S:
+		#if the character occured first time then add it to string M
+		if(keys not in dict1.keys()):
+			dict1[keys]=dict1.get(keys,0)+1
+		        M=M+keys
+
+	return M
+
