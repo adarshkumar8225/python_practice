071db614443ecf86b9e97591e76fe2786c95720f
Adarsh Kumar <51961214+adarshkumar8225@users.noreply.github.com>
2020-10-21 16:50:07 +0530

    anagram updated

--- anargam.py
+++ anargam.py
@@ -8,31 +8,23 @@ t=int(input())
 res={}
 result={}
 for i in range(0,t):
-    (str_first)=str((input())
-    print(5)
-    print(str_second)
+    #to take input two string seperated by spaces in same line
+    m,n=map(str,input().split())
+    #used to count the frequency of each char in the string1
+    for keys in m:
+        #if keys are present then get() return the value and 1 is added
+        #if keys are not present then key is created and value is added to 1
+        res[keys]=res.get(keys,0)+1
     
+    #used to count the frequency of each char in string2    
+    for key in n:
+        result[key]=result.get(key,0)+1
     
-    #all_freq = {} 
-  
-    #for keys in str_first: 
-        
-    #    res[keys] = res.get(keys, 0) + 1
-  
-    
-    #all_freqn = {} 
-  
-    #for keys in str_second: 
-        
-    #    result[keys] = result.get(keys, 0) + 1
-  
-    
-    
-    if(res==all_freqn) :
+    #if both dictionary contains same frequency and same characters
+    if(res==result):
         print("YES")
-    else:
+    else :
         print("NO")
         
-    res.clear()
+    res.clear()#clear dictionary for next iteration.
     result.clear()
-
